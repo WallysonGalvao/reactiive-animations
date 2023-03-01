99c5ce2af8e6639bfdbfcc4cb8bb36c3da57fb63
Wallyson Galvão <wallyson.galvao@sevenapps.com.br>
2023-02-28 21:55:00 -0300

    feat: 02-interpolate-with-scrollview

--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { Introduction } from "./components/00-introduction-to-reanimated-2";
 import { PanGestureBasics } from "./components/01-pan-gesture-handler-basics";
+import { AnimatedScrollView } from "./components/02-interpolate-with-scrollview";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <PanGestureBasics />
+      <AnimatedScrollView />
     </View>
   );
 }
---
+++ components/02-interpolate-with-scrollview/index.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+import { Page } from "./Page";
+
+const WORDS = ["What's", "up", "mobile", "devs?"];
+
+export const AnimatedScrollView = () => {
+  const translateX = useSharedValue(0);
+
+  const onScroll = useAnimatedScrollHandler((event) => {
+    translateX.value = event.contentOffset.x;
+  });
+
+  return (
+    <Animated.ScrollView
+      horizontal
+      pagingEnabled
+      onScroll={onScroll}
+      scrollEventThrottle={16}
+      showsHorizontalScrollIndicator={false}
+      style={styles.container}
+    >
+      {WORDS.map((title, index) => (
+        <Page
+          key={index.toString()}
+          title={title}
+          index={index}
+          translateX={translateX}
+        />
+      ))}
+    </Animated.ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
