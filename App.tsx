80c1f1f9659e7c2b57442dc406a2d79f87c00c37
Wallyson Galvão <wallyson.galvao@sevenapps.com.br>
2023-03-02 21:10:00 -0300

    feat: InterporlateColors

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import { StyleSheet, View } from "react-native";
 import { Introduction } from "./components/00-introduction-to-reanimated-2";
 import { PanGestureBasics } from "./components/01-pan-gesture-handler-basics";
 import { AnimatedScrollView } from "./components/02-interpolate-with-scrollview";
+import { InterporlateColors } from "./components/03-interpolate-color";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <AnimatedScrollView />
+      <InterporlateColors />
     </View>
   );
 }
