df79c31ab7b6349fe78ca93dbf8a00550d5228d4
Wallyson Galvão <wallyson.galvao@sevenapps.com.br>
2023-08-16 14:23:18 -0300

    feat: 04-pinch-gesture-handler-basics

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Introduction } from "./components/00-introduction-to-reanimated-2";
 import { PanGestureBasics } from "./components/01-pan-gesture-handler-basics";
 import { AnimatedScrollView } from "./components/02-interpolate-with-scrollview";
 import { InterporlateColors } from "./components/03-interpolate-color";
+import { PinchGestureHandlerBasics } from "./components/04-pinch-gesture-handler-basics";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <InterporlateColors />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <PinchGestureHandlerBasics />
+      </GestureHandlerRootView>
     </View>
   );
 }
